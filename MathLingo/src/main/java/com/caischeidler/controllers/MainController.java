c18e0287d90977ed1f84ac324f68c01b735049d5
slipperee_slime <scheidlercai@gmail.com>
2024-05-24 11:30:47 -0700

    layout not working

--- MathLingo/src/main/java/com/caischeidler/controllers/MainController.java
+++ MathLingo/src/main/java/com/caischeidler/controllers/MainController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 	
-	String[] SYMBOL_PATHS = {"multiply","subtract","plus","division"};
+	String[] SYMBOL_PATHS = {"images/multiply","images/subtract","images/plus","images/division"};
 	
 	@GetMapping("")
 	public String mainPage(Model model) {
@@ -29,14 +29,19 @@ public class MainController {
 		
 		model.addAttribute("symbolList", symbolList);
 		model.addAttribute("SYMBOL_PATHS", SYMBOL_PATHS);
-		return "mainPage";
+		return "mainPageSmall";
 	}
 	
-	@GetMapping("yo")
+	@GetMapping("game")
 	public String homePage(Model model) {
-		model.addAttribute("rating",6);
-		//This is a comment for the sake of checking to see if GitHub will work with this setup lol
-		return "homePage";
+		//Remember PostMapping and DataValidation from Data validation in Spring Boot tutorial and use that to get input for game
+		//make input object to save game state 
+		//use thymeleaf switch case statements to lock certain board sections
+		//have setable number of board sections
+		//To make use of layouts, create a header which has light and dark mode functionality and then make the other parts of the site bodies 
+		//Maybe even try to make the background automatic across all pages including the game page but overlay it with a rounded square box of some sort
+		//in the class for gamestate have variable for color of square and set it by that color in thymeleaf (possibly directly through inline css attribute)
+		return "gamePage";
 	}
 	
 	
